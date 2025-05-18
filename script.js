965a4bb442b460201f2c66236d0ee196a25c26b1
angel <panaderorodriguezangel.sangrina@gmail.com>
2025-05-18 14:22:49 +0200

    readme

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ fetch('https://moira-back.vercel.app/api')
   .then(response => response.json())
   .then(data => {
     console.log('Conexión exitosa:', data);
-    document.getElementById('resultado').textContent = '✅ Conectado con el backend';
+    document.getElementById('resultado').textContent = 'Conectado con el backend';
   })
   .catch(error => {
     console.error('Error al conectar con el backend:', error);
-    document.getElementById('resultado').textContent = '❌ Error de conexión';
+    document.getElementById('resultado').textContent = 'Error de conexión';
   });
